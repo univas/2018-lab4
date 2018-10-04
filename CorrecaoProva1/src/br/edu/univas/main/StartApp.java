2f96c6b0ea21c3274aee2fe5228acb54af12a116
Rodrigo Luis de Faria <rodrigo.faria@inatel.br>
2018-10-04 20:03:53 -0300

    Correção Prova 1

---
+++ CorrecaoProva1/src/br/edu/univas/main/StartApp.java
@@ -0,0 +1,12 @@
+package br.edu.univas.main;
+
+import br.edu.univas.frame.CalculadoraFrame;
+
+public class StartApp {
+
+	public static void main(String[] args) {
+		CalculadoraFrame frame = new CalculadoraFrame();
+		frame.setVisible(true);
+	}
+	
+}
